43b56ff9ce8889321ce81e9c3f9204b2c4adeb86
andyw324 <andrew.wong@capgemini.com>
2017-03-07 01:39:32 +0000

    cleaned and fixed for querying using dates

--- queries.js
+++ queries.js
@@ -41,16 +41,9 @@ function getSingleWard(req, res, next) {
 }
 
 function getQueryResults(req, res, next) {
-  // var fieldList = {wd15cd, category, location_type, longitude, latitude, street_name, 
-  //      street_id, location_subtype, outcome_category, outcome_date, 
-  //      persistent_id, month, context, id};
-  
+
   // -------------- Code to create a where clause for a SQL SELECT Statement
 
-  // var id = request.query.id;
-  // var name = request.query.name;
-  // var test = request.query.test;
-  //var fieldList = ['a','test','c','name','e','id','g'];
   var fieldList = {'Place Name':'string', 
         'category':'string',
         'location_type':'string',
@@ -67,75 +60,44 @@ function getQueryResults(req, res, next) {
         'id':'integer'};
 
   var whereClause = '';
-  var whereValue;// = '';
-  var symbol;// = '';
-  var dataType;// = '';
-  // response.end('Name: ' + name + '\nid: ' + id + '\nTest: ' + test);
-  console.log(req.query);
-  // console.log(fieldList);
+  var whereValue;
+  var symbol;
+  var dataType;
+  var typePrefix = '';
 
   for (field in fieldList){
-    // console.log(field + " type: " + fieldList[field]);
+    
     whereValue = req['query'][field];
     dataType = fieldList[field];
-    // console.log(dataType);
+    
     switch (dataType)
     {
       case 'string':
         symbol = "'";
-        // console.log(field + ' type = string');
+        typePrefix = '';
         break;
       case 'date':
-        symbol = '#';
-        // console.log(field + ' type = date - ' + symbol);
+        symbol = "'";
+        typePrefix = ' date ';
         break;
       default:
-        // console.log(field + ' type = default')
        symbol = '';
+       typePrefix = '';
     }
-    // console.log('1-symbol: ' + symbol);
-    // console.log('1-WhereValue: ' + whereValue);
-    // console.log('1-whereClause: ' + whereClause);
+
     if (whereClause != '' && !(typeof whereValue === 'undefined')){ //'undefined')){
       whereClause += ' AND ';
     };
-    // console.log('2-symbol: ' + symbol);
-    console.log('2-WhereValue: ' + whereValue);
-    // console.log('2-whereClause: ' + whereClause);
+
     if (!(typeof whereValue === 'undefined')){
-      whereClause += '"' + field + '" = ' + symbol + decodeURIComponent(whereValue) + symbol;
+      whereClause += '"' + field + '" = ' + typePrefix + symbol + decodeURIComponent(whereValue) + symbol;
     };
-    // console.log('3-symbol: ' + symbol);
-    // console.log('3-WhereValue: ' + whereValue);
-    // console.log('3-whereClause: ' + whereClause);
-    // console.log('Where Statement: ' + whereClause);
-    // console.log(item + ': ' + req['query'][item]);
-  
   }
 
   if (whereClause != '') {
     whereClause = ' WHERE ' + whereClause
   };
-  // fieldList.forEach(function(item,index,array){
-  //   var whereValue = req['query'][item];
-  //   console.log(typeof whereValue);
-  //   // console.log(whereValue);
-  //   if (whereClause != '' && !(typeof whereValue === 'undefined')){ //'undefined')){
-  //     whereClause += ' AND ';
-  //   };
-  //   if (!(typeof whereValue === 'undefined')){
-  //     whereClause += item + ' = ' + req['query'][item];
-  //   };
-  //   // console.log(item + ': ' + req['query'][item]);
-  // });
-  // if (whereClause != '') {
-  //   whereClause = ' WHERE ' + whereClause
-  // };
 
-
-  // console.log('whereClause: ' + whereClause);
-
-  // response.end(whereClause);
   // // ----------------- end of Where Clause statement generation
 
   console.log('Query String: select * from crime_stats' + whereClause);
@@ -156,63 +118,8 @@ function getQueryResults(req, res, next) {
 
 
 
-
-// function createPuppy(req, res, next) {
-//   req.body.age = parseInt(req.body.age);
-//   db.none('insert into pups(name, breed, age, sex)' +
-//       'values(${name}, ${breed}, ${age}, ${sex})',
-//     req.body)
-//     .then(function () {
-//       res.status(200)
-//         .json({
-//           status: 'success',
-//           message: 'Inserted one puppy'
-//         });
-//     })
-//     .catch(function (err) {
-//       return next(err);
-//     });
-// }
-
-// function updatePuppy(req, res, next) {
-//   db.none('update pups set name=$1, breed=$2, age=$3, sex=$4 where id=$5',
-//     [req.body.name, req.body.breed, parseInt(req.body.age),
-//       req.body.sex, parseInt(req.params.id)])
-//     .then(function () {
-//       res.status(200)
-//         .json({
-//           status: 'success',
-//           message: 'Updated puppy'
-//         });
-//     })
-//     .catch(function (err) {
-//       return next(err);
-//     });
-// }
-
-// function removePuppy(req, res, next) {
-//   var pupID = parseInt(req.params.id);
-//   db.result('delete from pups where id = $1', pupID)
-//     .then(function (result) {
-//       /* jshint ignore:start */
-//       res.status(200)
-//         .json({
-//           status: 'success',
-//           message: `Removed ${result.rowCount} puppy`
-//         });
-//       /* jshint ignore:end */
-//     })
-//     .catch(function (err) {
-//       return next(err);
-//     });
-// }
-
-
 module.exports = {
   getAllWards: getAllWards,
   getSingleWard: getSingleWard,
-  getQueryResults: getQueryResults //,
-  // createPuppy: createPuppy,
-  // updatePuppy: updatePuppy,
-  // removePuppy: removePuppy
+  getQueryResults: getQueryResults 
 };
